b6b88f756dbc4bec931e81e5bfc0513f355cee37
Amatisme <publicmat@me.com>
2020-02-11 11:10:08 -0500

    Spelling error in comments.

--- dcp-20200211.js
+++ dcp-20200211.js
@@ -1,5 +1,5 @@
-// Given a list of numbers ad a number 'k'. return whether any two
-// numbers from this list add up to 'k'
+// Given a list of numbers and a number 'k'. Return whether any two
+// numbers from this list add up to 'k'.
 let numbers = [10,15,3,7];
 let k = 17;
 console.log(numbers.map(item => {

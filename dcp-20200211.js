e5ff2e97279a98caacd1e01f91efafb0c1618751
Amatisme <publicmat@me.com>
2020-02-12 14:40:03 -0500

    Added URL to CGP version.

--- dcp-20200211.js
+++ dcp-20200211.js
@@ -1,6 +1,8 @@
-/* Given an array of integers, return a new array such that each element
-at index i of the new array is the product of all the numbers in the
-original array except the one at i. */
+/*
+ * Given an array of integers, return a new array such that each element
+ * at index i of the new array is the product of all the numbers in the
+ * original array except the one at i.
+ */
 
 // define set of numbers
 let numbers = [1,2,3,4,5];
@@ -14,3 +16,11 @@ console.log(numbers.map(item => {
   // return product of array values
   return j.reduce((acc,l) => acc * l);
 }));
+
+/*
+ * Run the Google Cloud Function version from the url below.
+ * Feel free to change the numbers array values.
+ *
+ * https://us-central1-pivotes5.cloudfunctions.net/dcp-20200211?numbers=1,2,3,4,5
+ *
+*/
